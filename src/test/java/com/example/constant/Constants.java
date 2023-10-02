41e22c6c01b72a0e0b17aa6c91e5dc135db145f0
Ivan Krutikov <infr4rot@gmail.com>
2023-10-02 10:59:37 +0700

    feline test

--- src/test/java/com/example/constant/Constants.java
+++ src/test/java/com/example/constant/Constants.java
@@ -6,5 +6,7 @@ public class Constants {
     public static final List<String> HERBIVORE_FOODS = List.of("Трава", "Различные растения");
     public static final List<String> PREDATOR_FOODS = List.of("Животные", "Птицы", "Рыба");
     public static final String EXPECTED_FAMILY = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+    public static final String FELINE_FAMILY = "Кошачьи";
+
 
 }
